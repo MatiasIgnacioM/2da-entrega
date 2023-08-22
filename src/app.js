25d71194651570c8b0c455fb8bd9b85e65c9ca77
Matias Ignacio MV <matiasignacio.mv96@gmail.com>
2023-08-21 21:03:58 -0400

    !

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import  ProductManager  from "./productosManager.js"; 
+import { ProductManager } from './productManager.js'
+
 import express from 'express'
 
 const app = express()
@@ -7,8 +8,9 @@ const productManager = new ProductManager("./data/products.json")
 app.get('/products', async (req,res)=>{
     const result = await productManager.getProducts()
     const limit = req.query.limit
-    if (typeof result === 'object' && 'status' in result) {
-        return res.status(result.status).json({ error: result.message });
+    if (typeof result == 'string') {
+        const error = result.split(' ')
+        return res.status(parseInt(error[0].slice(1,4))).json({error: result.slice(6)})
     }
     res.status(200).json({ payload: result.slice(0, limit) });
 })
