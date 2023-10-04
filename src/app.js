35a7a008178397366fcc8f69b5b033b9483aac39
Matias Ignacio MV <matiasignacio.mv96@gmail.com>
2023-10-04 15:22:01 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,35 +1,46 @@
 import express from 'express'
-import { Server } from 'socket.io'
 import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+import Sockets from './sockets.js'
+import mongoose from 'mongoose'
 import productsRouter from './routers/product.router.js'
 import cartsRouter from './routers/cart.router.js'
 import viewsRouter from './routers/view.router.js'
-import ProductManager from '../src/dao/filesystem/productManager.js'
+import chatRouter from './routers/chat.router.js'
+
+const MONGO_URI = 'mongodb://localhost:27017'
+const MONGO_DB_NAME = 'monoschinos'
+export const PORT = 8080
 
 const app = express()
-
-app.use(express.json()) 
-app.use('/', express.static('./src/public'))
+app.use(express.json())
+app.use(express.static('./src/public'))
 app.engine('handlebars', handlebars.engine())
 app.set('views', './src/views')
 app.set('view engine', 'handlebars')
 
-app.get('/', async (req, res) => {
-  const productManager = new ProductManager("./data/products.json")
-  const products = await productManager.getProducts()
-  res.render("index.handlebars",{products})
-})
+try {
+    await mongoose.connect(MONGO_URI, { 
+        dbName: MONGO_DB_NAME,
+        useUnifiedTopology: true
+    })
+    console.log('DB connected!')
+    const server = app.listen(PORT, () => console.log('Server Up'))
+    const io = new Server(server)
+    app.use((req, res, next) => {
+        req.io = io
+        next()
+    })
 
-app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
-app.use('/products', viewsRouter)
+    app.get('/', (req, res) => res.render('index'))
+    app.use('/api/products', productsRouter)
+    app.use('/api/carts', cartsRouter)
+    app.use('/products', viewsRouter)
+    app.use('/carts', viewsRouter)
+    app.use("/chat", chatRouter)
 
-const httpServer = app.listen(8080, () => console.log('Server up'))
-const io = new Server(httpServer)
-
-io.on("connection", (socket) => {
-  console.log(`New client connected`)
-  socket.on('productList', (data) => {
-    io.emit('updatedProducts', (data))
-  })
-})
+    Sockets(io)
+} catch(err) {
+    console.log('Cannot connect to DB :(  ==> ', err.message)
+    process.exit(-1)
+}
